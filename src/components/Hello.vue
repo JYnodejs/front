3a6fa5271336a67aba413f6a3d4b35b2692a5ecb
nodejs <296305971@qq.com>
2017-05-12 21:15:35 +0800

    初始化Element项目测试

---
+++ src/components/Hello.vue
@@ -0,0 +1,25 @@
+<template>
+  <div >
+     <h1>{{msg}}</h1>
+     <el-button type="success" v-on:click="login" icon="el-icon-check">登录</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    login () {
+      this.$message({
+        message: '恭喜您登陆成功',
+        type: 'success'
+      })
+    }
+  }
+}
+</script>
